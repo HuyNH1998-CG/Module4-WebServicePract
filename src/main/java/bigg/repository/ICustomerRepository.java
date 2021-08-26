eacfa77548a0c2d8d5735309e4b5bb7fa12035a1
zhuy-nguyenz <graio911@gmail.com>
2021-08-26 09:33:17 +0700

    1st

---
+++ src/main/java/bigg/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package bigg.repository;
+
+import bigg.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/bigg/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package bigg.service;
+
+import bigg.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
